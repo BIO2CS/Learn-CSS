99b87d1636e446c0903acdba4482459c211ea9e6
Huiqiong Gu <hgu@buffalo.edu>
2021-12-20 17:20:37 -0800

    fix prettier to use spaces instead of tabs

--- 025_Transitions/script.js
+++ 025_Transitions/script.js
@@ -1,10 +1,10 @@
 let transitionBox = document.querySelector("#has-transition");
 
 transitionBox.addEventListener("change", (event) => {
-	let squareBox = document.querySelector(".square");
-	if (!event.target.checked) {
-		squareBox.classList.remove("has-transition");
-	} else {
-		squareBox.classList.add("has-transition");
-	}
+  let squareBox = document.querySelector(".square");
+  if (!event.target.checked) {
+    squareBox.classList.remove("has-transition");
+  } else {
+    squareBox.classList.add("has-transition");
+  }
 });
